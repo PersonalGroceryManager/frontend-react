45c70ef1cc699c7268fb0271a45b0f5e09045d75
Gai <107824428+GZwong@users.noreply.github.com>
2025-01-18 23:41:02 +0000

    fix: BrowserRouter -> HashRouter
    Change to be compatible with GH pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// BrowserRouter replaced by HashRouter for compatibility with GitHub pages
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
